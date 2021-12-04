6f34321827051342e66443a0d3dc818c352e2b09
Yigithan Guven <demirejderha@gmail.com>
2021-12-04 16:11:19 +0300

    Inputs_Updated

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -7,11 +7,10 @@ public class CarController : MonoBehaviour
     public Rigidbody rb;
 
     
-    Vector3 rotationRight = new Vector3(0, 60, 0);
-    Vector3 rotationLeft = new Vector3(0, -60, 0);
+    Vector3 rotationRight = new Vector3(0, 70, 0);
+    Vector3 rotationLeft = new Vector3(0, -70, 0);
     Vector3 forward = new Vector3(0, 0, 1);
 
-    private bool isTouchedMobile=false;
     public float speed = 1.5f;
     public bool isFreezed = true;
     public bool isControlleble = true;
@@ -19,18 +18,75 @@ public class CarController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (isControlleble )
-        {
-            
-            MoveStraight();
 
-            if (Input.GetKey("d") )
+        if (SystemInfo.deviceType == DeviceType.Desktop)
+            DesktopControls();
+        else
+            MobileControls();
+        
+    }
+
+    void MoveLeft(){
+        Quaternion deltaRotationLeft = Quaternion.Euler(rotationLeft * Time.deltaTime);
+        rb.MoveRotation(rb.rotation * deltaRotationLeft);
+    }
+
+    void MoveRight(){
+        Quaternion deltaRotationRight = Quaternion.Euler(rotationRight * Time.deltaTime);
+        rb.MoveRotation(rb.rotation * deltaRotationRight);
+    }
+
+    void MoveStraight(){     
+       transform.Translate(forward * speed * Time.deltaTime);           
+    }
+
+    void MobileControls()
+    {
+        if (isControlleble)
+        {
+            if (!isFreezed)
+                MoveStraight();
+            if (Input.touchCount > 0)
+            {
+                var touch = Input.GetTouch(0);
+                if (touch.position.x < Screen.width / 2)
+                {
+                    MoveLeft();
+                    isFreezed = false;
+
+                }
+                else if (touch.position.x > Screen.width / 2)
+                {
+                    MoveRight();
+                    isFreezed = false;
+                }
+            }
+
+        }
+        else
+        {
+            if (!isFreezed)
+            {
+                //replay here
+            }
+        }
+    }
+
+    void DesktopControls()
+    {
+        if (isControlleble)
+        {
+
+            if (!isFreezed)
+                MoveStraight();
+
+            if (Input.GetKey("d"))
             {
                 MoveRight();
                 isFreezed = false;
             }
 
-            if (Input.GetKey("a") )
+            if (Input.GetKey("a"))
             {
                 MoveLeft();
                 isFreezed = false;
@@ -44,68 +100,5 @@ public class CarController : MonoBehaviour
                 //replay here
             }
         }
-
-
-        //Mobile Controls
-        /*if (isControlleble)
-        {
-            if(!isFreezed)
-                MoveStraight();
-            if (Input.touchCount > 0)
-            {
-                    var touch = Input.GetTouch(0);
-                if (touch.position.x < Screen.width / 2)
-                {
-                    MoveLeft();
-                    isFreezed = false;
-
-                }
-                else if (touch.position.x > Screen.width / 2)
-                {
-                    MoveRight();
-                    isFreezed = false;
-                }
-            }
-            
-        }
-        else
-        {
-            if (!isFreezed)
-            {
-                //replay here
-            }
-        }
-        
-        //Mobile Controls End
-        */
-
-
-
-
-
-
-
-
-
     }
-
-    void MoveLeft(){
-        Quaternion deltaRotationLeft = Quaternion.Euler(rotationLeft * Time.deltaTime);
-        rb.MoveRotation(rb.rotation * deltaRotationLeft);
-    }
-
-    void MoveRight(){
-        Quaternion deltaRotationRight = Quaternion.Euler(rotationRight * Time.deltaTime);
-        rb.MoveRotation(rb.rotation * deltaRotationRight);
-    }
-
-    void MoveStraight(){
-        if (!isFreezed)
-        {
-            transform.Translate(forward * speed * Time.deltaTime);
-            
-        }
-       
-    }
-
 }
